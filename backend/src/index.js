ff8c52b30945e09e8e9c900bd84832e276ef16dc
Giovanni Pires da Silva <giovanni@somosgad.com>
2020-03-28 17:58:30 -0300

    Decouple routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,49 +1,9 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
-
-/**
- * Rota / Recurso
- */
-
-/**
- * Métodos HTTP:
- * 
- * GET: Buscar/listar uma informação do back-end
- * POST: Criar uma informação no back-end
- * PUT: Alterar uma informação no back-end
- * DELETE: Deletar uma informação no back-end
- */
-
-/**
- * Tipos de parâmetros:
- * 
- * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
- * Route Params: Parâmetros utilizados para identificar recursos
- * Request body: Corpo da requisição, utilizado para criar ou alterar recursos
- */
-
-/**
- * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
- * MoSQL: MongoDB, CouchDB, etc
- */
-
-/**
- * Driver: SELECT * FROM users
- * Query Builder: table('users'). select('*').where()
- */
-
-app.post('/users', (request, response) => {
-  const body = request.body;
-
-  console.log(body);
-
-  return response.json({
-    evento: 'Semana OmniStack 11.0',
-    aluno: 'Cleiton Souza'
-  });
-});
+app.use(routes);
 
 app.listen(3333);
