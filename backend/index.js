4b286beeb90ec5c55ce52374284ffffebe02783c
Giovanni Pires da Silva <giovanni@somosgad.com>
2020-03-28 17:36:48 -0300

    Query params sample

--- backend/index.js
+++ backend/index.js
@@ -9,13 +9,25 @@ const app = express();
 /**
  * Métodos HTTP:
  * 
- * GET: Buscar uma informação do back-end
+ * GET: Buscar/listar uma informação do back-end
  * POST: Criar uma informação no back-end
  * PUT: Alterar uma informação no back-end
  * DELETE: Deletar uma informação no back-end
  */
 
-app.post('/users', (request, response) => {
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request body:
+ */
+
+app.get('/users', (request, response) => {
+  const params = request.query;
+
+  console.log(params);
+
   return response.json({
     evento: 'Semana OmniStack 11.0',
     aluno: 'Giovanni Pires da Silva'
