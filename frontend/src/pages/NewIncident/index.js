1a07ce775f7b82208e9d0920923dd4bc4ccc7c66
Giovanni Pires da Silva <giovanni@somosgad.com>
2020-03-29 02:43:53 -0300

    Fix missing letter

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -46,7 +46,7 @@ export default function NewIncident() {
           <img src={logoImg} alt="Be The Hero"/>
 
           <h1>Cadastrar novo caso</h1>
-          <p>Descreva o caso detalhadamente para encontrar m herói para resolver isso.</p>
+          <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso.</p>
 
           <Link className="back-link" to="/profile">
             <FiArrowLeft size={16} color="#E02041" />
