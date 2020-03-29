115928156a51b50c242e18444199c4041591d0c2
Giovanni Pires da Silva <giovanni@somosgad.com>
2020-03-29 00:13:14 -0300

    Handle delete without updating the list

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -24,6 +24,18 @@ export default function Profile() {
     })
   }, [ongId]);
 
+  async function handleDeleteIncident(id) {
+    try {
+      await api.delete(`incidents/${id}`, {
+        headers: {
+          Authorization: ongId,
+        }
+      }); 
+    } catch (err) {
+      alert('Erro ao deletar caso, tente novamente.');
+    }
+  }
+
   return (
     <div className="profile-container">
       <header>
@@ -50,7 +62,7 @@ export default function Profile() {
             <strong>VALOR:</strong>
             <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}</p>
 
-            <button type="button">
+            <button onClick={() => handleDeleteIncident(incident.id)} type="button">
               <FiTrash2 size={20} color="#a8a8b3" />
             </button>
           </li>
