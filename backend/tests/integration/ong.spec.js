a5659a06ba6c4b0d5d6ae89e61138ae808405071
Giovanni Pires da Silva <giovanni@somosgad.com>
2020-03-29 16:58:46 -0300

    Fix integration test
    - Add migrations before each test
    - Fix expect of that test

--- backend/tests/integration/ong.spec.js
+++ backend/tests/integration/ong.spec.js
@@ -1,7 +1,12 @@
 const request = require('supertest');
 const app = require('../../src/app');
+const connection = require('../../src/database/connection');
 
 describe('ONG', () => {
+  beforeEach(async () => {
+    await connection.migrate.latest();
+  });
+
   it('should be able to create a new ONG', async () => {
     const response = await request(app)
       .post('/ongs')
@@ -13,6 +18,7 @@ describe('ONG', () => {
         uf: 'RS',
       });
 
-    console.log(response.body);
+    expect(response.body).toHaveProperty('id');
+    expect(response.body.id).toHaveLength(8);
   });
 })
