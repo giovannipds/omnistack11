fa440046f6e22bda12119fbba87a2c337ad38790
Giovanni Pires da Silva <giovanni@somosgad.com>
2020-03-29 03:22:49 -0300

    Remove all styles

--- mobile/App.js
+++ mobile/App.js
@@ -1,25 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello OmniStack</Text>
+    <View>
+      <Text>Hello OmniStack</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#e02041',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: {
-    color: '#fff',
-    fontSize: 20,
-    fontWeight: 'bold'
-  },
-});
