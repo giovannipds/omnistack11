edc16a69f8d2f07d3ffb51b2d175f72b8fe23935
Giovanni Pires da Silva <giovanni@somosgad.com>
2020-03-29 03:58:42 -0300

    Restore app, create routes and view something

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import logoImg from '../../assets/logo.png';
+
+import styles from './styles';
+
 export default function Incidents() {
   return (
-    <View />
+    <View style={styles.container}>
+    </View>
   );
 }
