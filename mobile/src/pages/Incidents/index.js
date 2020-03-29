acd27ac931f1b70bff8731e8fb3cf1e04c48f5b0
Giovanni Pires da Silva <giovanni@somosgad.com>
2020-03-29 04:08:32 -0300

    Add expo-contants and use it to pad from status bar

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image, Text } from 'react-native';
 
 import logoImg from '../../assets/logo.png';
 
@@ -8,6 +8,15 @@ import styles from './styles';
 export default function Incidents() {
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+        <Text style={styles.headerText}>
+          Total de <Text style={styles.headerTextBold}>0 casos</Text>.
+        </Text>
+      </View>
+
+      <Text style={styles.title}>Bem-vindo!</Text>
+      <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia.</Text>
     </View>
   );
 }
