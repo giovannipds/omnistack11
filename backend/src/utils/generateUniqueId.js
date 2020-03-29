9007742ded3473a6d588bebfb0410ed5a3e32d71
Giovanni Pires da Silva <giovanni@somosgad.com>
2020-03-29 16:35:50 -0300

    Fix generateUniqueId test

--- backend/src/utils/generateUniqueId.js
+++ backend/src/utils/generateUniqueId.js
@@ -1,9 +1,5 @@
-const id = crypto.randomBytes(4).toString('HEX');
-
-const id = crypto.randomBytes(4).toString('HEX');
-
 const crypto = require('crypto');
 
-export default function generateUniqueId() {
+module.exports = function generateUniqueId() {
   return crypto.randomBytes(4).toString('HEX');
 }
